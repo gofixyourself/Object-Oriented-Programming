9f5d98a9be6d20a39246913d1ea08feff74b8890
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-03-17 20:56:45 +0300

    Added function descriptions

--- Viewer/based operations for dot.cpp
+++ Viewer/based operations for dot.cpp
@@ -1,19 +1,20 @@
 #include <cstdlib>
-#include "types.h"
+#include "strcutures of types.h"
 #include "errors.h"
 
 // Initializing a point:
-void initialization(struct arrayOfDots &ourArray)
+void initialization(arrayOfDots &ourArray)
 {
     ourArray.array = 0;
     ourArray.amount = 0;
     ourArray.index = 0;
 }
 
-// Comment
-int createArrayOfDots(struct arrayOfDots &ourArray, int size)
+// Creating an array (allocating memory for it,
+// initializing the index and the number of elements in it):
+int createArrayOfDots(arrayOfDots &ourArray, int size)
 {
-    ourArray.array = (struct dot*)malloc(size * sizeof(struct dot));
+    ourArray.array = (dot*)malloc(size * sizeof(dot));
 
     if(!ourArray.array)
         return MEMORY_ALLOCATION_ERROR;
@@ -24,12 +25,14 @@ int createArrayOfDots(struct arrayOfDots &ourArray, int size)
     return ALL_IS_WELL;
 }
 
-// Comment
-int insertDot(struct arrayOfDots&ourArray, struct dot ourDot)
+// For adding a point to an array:
+int insertDot(arrayOfDots&ourArray, dot ourDot)
 {
     ourArray.array[ourArray.index++] = ourDot;
 
     return ALL_IS_WELL;
 }
+
+
 
 
