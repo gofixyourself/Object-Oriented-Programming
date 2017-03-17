081ae951d6e56d945feccd9b3a2aa05e32169543
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-03-17 21:08:44 +0300

    Added a header for a file with based operations for a dot

---
+++ Viewer/based operations for dot.h
@@ -0,0 +1,8 @@
+#ifndef VIEWER_BASED_OPERATIONS_FOR_DOT_H
+#define VIEWER_BASED_OPERATIONS_FOR_DOT_H
+
+void initialization(arrayOfDots &ourArray);
+int createArrayOfDots(arrayOfDots &ourArray, int size);
+int insertDot(arrayOfDots&ourArray, dot ourDot);
+
+#endif //VIEWER_BASED_OPERATIONS_FOR_DOT_H
