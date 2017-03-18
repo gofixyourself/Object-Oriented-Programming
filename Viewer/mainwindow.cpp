30092c790184f6b9245f8df6614650f2df58cae2
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-03-18 22:59:02 +0300

    Added functions for closing and opening a program window

--- Viewer/mainwindow.cpp
+++ Viewer/mainwindow.cpp
@@ -3,18 +3,34 @@
 #include "strcutures of types.h"
 #include "errors.h"
 
+// Opening the program window:
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    struct action openWindow;
+    openWindow.toDo = OPEN;
+
+    checkProcess(openWindow);
+}
+
+// Opening a file:
 void MainWindow::openingAFile()
 {
     struct action opening;
 
-    opening.todo = LOAD;
+    // Indicate current state of the event:
+    opening.toDo = LOAD;
 
     struct loadingAFile inputFile;
 
+    // Read the name of file:
     inputFile.name = ui->name->text().toStdString().c_str();
 
+    // Loafing a file with the specified name:
     opening.load = inputFile;
 
+    // Checking action for possible errors:
     enum errors error = (enum errors)checkProcess(opening);
 
     if (error)
@@ -22,3 +38,16 @@ void MainWindow::openingAFile()
 
     on_scale_sliderMoved(50);
 }
+
+// Closing the program:
+MainWindow::closingAll()
+{
+    struct action closing;
+
+    closing.toDo = QUIT;
+
+    checkProcess(com);
+
+    delete ui;
+}
+
