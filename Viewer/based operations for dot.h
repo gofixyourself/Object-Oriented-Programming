79134fd70e5df28bd2c3ad32df60f7e7100a7a03
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-03-19 23:29:32 +0300

    Modified a header file

--- Viewer/based operations for dot.h
+++ Viewer/based operations for dot.h
@@ -4,5 +4,9 @@
 void initialization(arrayOfDots &ourArray);
 int createArrayOfDots(arrayOfDots &ourArray, int size);
 int insertDot(arrayOfDots&ourArray, dot ourDot);
+int popDot(dot &needfulDot, arrayOfDots &ourArray, int index);
+int settingDot(arrayOfDots &ourArray, dot &ourDot, int index);
+int isEmpty(arrayOfDots &ourArray);
+int clearingArrayDots(arrayOfDots &ourArray);
 
 #endif //VIEWER_BASED_OPERATIONS_FOR_DOT_H
