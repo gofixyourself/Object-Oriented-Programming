12b26c609a7474770cf30957bf0c3b857dc04008
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-03-19 22:05:49 +0300

    Added a header file for model operations

---
+++ Viewer/for model.h
@@ -0,0 +1,14 @@
+#ifndef VIEWER_FOR_MODEL_H
+#define VIEWER_FOR_MODEL_H
+
+#include "strcutures of types.h"
+
+struct model {
+     int x;
+     int y;
+
+     arrayOfDots dots;
+     arrayOfEdges edges;
+};
+
+#endif // VIEWER_FOR_MODEL_H
