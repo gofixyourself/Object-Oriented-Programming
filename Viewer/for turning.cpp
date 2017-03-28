7e302a182d705c739822436e91e0e30cef3620bd
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-03-29 00:50:24 +0300

    Fixed some errors

--- Viewer/for turning.cpp
+++ Viewer/for turning.cpp
@@ -1,29 +1,29 @@
 #include "for turning.h"
 
-// -----------------------------For horisontal rotation-----------------------------
-int forHorisontalRotation(arrayOfDots ourArray, turningAModel angle)
+// -----------------------------For horizontal rotation-----------------------------
+int forHorizontalRotation(arrayOfDots ourArray, turningAModel angle)
 {
     int forError = 0;
 
     for (int i = 0; (i < ourArray.index) && (!forError); i++)
-        forError = dotTurnLeft(ourArray, i, angle.displacement);
+        forError = dotTurnHorizontal(ourArray, i, angle.displacement);
 
     return forError;
 }
 
-int dotTurnHorisontal(arrayOfDots ourArray, int index, double displacement)
+int dotTurnHorizontal(arrayOfDots ourArray, int index, double displacement)
 {
-    horisontalRotation(ourArray.array[index], displacement);
+    horizontalRotation(ourArray.array[index], displacement);
 
     return NONE;
 }
 
-void horisontalRotationDot(dot &ourDot, double angle)
+void horizontalRotationDot(dot &ourDot, double angle)
 {
     double degreeToRadian = angle * PI_CONST / 180;
     double buffer = ourDot.x;
 
-    ourDot.x = buffer * cos(degreeToRad) - ourDot.y * sin(degreeToRadian);
+    ourDot.x = buffer * cos(degreeToRadian) - ourDot.y * sin(degreeToRadian);
     ourDot.y = buffer * sin(degreeToRadian) + ourDot.y * cos(degreeToRadian);
     ourDot.z = ourDot.z;
 }
@@ -41,7 +41,7 @@ int forVerticalRotation(arrayOfDots ourArray, turningAModel angle)
     return forError;
 }
 
-int dotTurnVertical(arrayOfDots ourArray, int index ,double angle)
+int dotTurnVertical(arrayOfDots ourArray, int index, double angle)
 {
     verticalRotationDot(ourArray.array[index], angle);
 
