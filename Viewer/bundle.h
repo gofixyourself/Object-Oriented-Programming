1b6313b71d783d89f48a7e7a738176fdaaf39b08
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-04-17 12:24:51 +0300

    Added a header file for bundle.cpp

---
+++ Viewer/bundle.h
@@ -0,0 +1,10 @@
+#ifndef BUNDLE_H
+#define BUNDLE_H
+
+#include "strcutures of types.h"
+#include "errors.h"
+#include "edge.h"
+
+int bundleDots(arrayOfEdges &ourEdges, arrayOfDots ourDots, FILE* inputFile);
+
+#endif // BUNDLE_H
