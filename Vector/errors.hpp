905ad2590e1f4294a183e6c0a46039eea0cd4893
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-05-02 01:49:36 +0300

    Added errors classes for wrong input, memory allocation and etc

--- Vector/errors.hpp
+++ Vector/errors.hpp
@@ -3,4 +3,29 @@
 
 #include <exception>
 
+class SomeError : public std::exception {
+public:
+    virtual const char *what() {
+        return "Ooops!";
+    }
+};
+
+class MemoryError : public SomeError {
+public:
+    const char *what() {
+        return "Problems with memory allocation.";
+    }
+};
+
+class InputSizeError: public SomeError {
+public:
+    const char *what() {
+        return "You entered a number less than zero. Try again.";
+    }
+};
+
+
+
+
+
 #endif // ERRORS_HPP
