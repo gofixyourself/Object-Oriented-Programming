fa1b8bd36f36e21e11491178ecce400c1f48a114
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-05-02 13:51:45 +0300

    Added namespace for mathematical vector

--- Vector/namespace.hpp
+++ Vector/namespace.hpp
@@ -1,4 +1,24 @@
 #ifndef NAMESPACE_HPP
 #define NAMESPACE_HPP
 
+#include <cstddef>
+#include <iostream>
+
+namespace mathvector {
+    namespace essential {
+        template<class T>
+        class VectorIterator;
+
+        class VectorContainer;
+
+        template<class T>
+        class MathematicalVector;
+    }
+    template <class T>
+    class ConstIterator;
+
+    template <class T>
+    class NonConstantIterator;
+}
+
 #endif // NAMESPACE_HPP
