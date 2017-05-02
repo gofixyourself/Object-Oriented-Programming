c774931730b8c4fcf1db14e6f92c001bdc824948
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-05-02 20:59:44 +0300

    Added essential container implementation

--- Vector/for essential container.hpp
+++ Vector/for essential container.hpp
@@ -1,4 +1,26 @@
 #ifndef FOR_ESSENTIAL_CONTAINER_HPP
 #define FOR_ESSENTIAL_CONTAINER_HPP
 
+#include "essential container.hpp"
+
+namespace mathvector {
+    namespace essential {
+        EssentialContainer::EssentialContainer(): content(0) {}
+
+        EssentialContainer::EssentialContainer(size_t extent): content(extent) {}
+
+        EssentialContainer::~EssentialContainer() {
+            extent = 0;
+        }
+
+        bool EssentialContainer::check_is_empty() const {
+            return extent == 0;
+        }
+
+        size_t EssentialContainer::size() const {
+           return extent;
+       }
+    }
+}
+
 #endif // FOR_ESSENTIAL_CONTAINER_HPP
