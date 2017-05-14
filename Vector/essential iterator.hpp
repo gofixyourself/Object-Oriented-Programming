e79edbfbceb890565205432fd7d682cc61b7e8bd
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-05-14 23:24:22 +0300

    Removed unnecessary namespace and renamed constructor

--- Vector/essential iterator.hpp
+++ Vector/essential iterator.hpp
@@ -5,31 +5,29 @@
 #include "for essential iterator.hpp"
 
 namespace mathvector {
-    namespace essential {
-        template<class T>
-        class EssentialIterator {
-        public:
-            essential_iterator(const EssentialIterator &element);
-            EssentialIterator &operator = (const EssentialIterator &element);
+    template<class T>
+    class EssentialIterator {
+    public:
+        EssentialIterator(const EssentialIterator &element);
+        EssentialIterator &operator = (const EssentialIterator &element);
 
-            EssentialIterator &operator++();
-            EssentialIterator &operator--();
+        EssentialIterator &operator++();
+        EssentialIterator &operator--();
 
-            EssentialIterator operator++(int);
-            EssentialIterator operator--(int);
+        EssentialIterator operator++(int);
+        EssentialIterator operator--(int);
 
-            bool operator == (const EssentialIterator &element);
-            bool operator != (const EssentialIterator &element);
+        bool operator == (const EssentialIterator &element);
+        bool operator != (const EssentialIterator &element);
 
-            ptrdiff_t operator - (const EssentialIterator &element);
+        ptrdiff_t operator - (const EssentialIterator &element);
 
-            virtual ~essential_iterator();
+        virtual ~EssentialIterator();
 
-        protected:
-            essential_iterator(T *pointer);
-            T *pointer;
-        };
-    }
+    protected:
+        EssentialIterator(T *pointer);
+        T *pointer;
+    };
 }
 
 #endif // ESSENTIAL_ITERATOR_HPP
--- Vector/for essential iterator.hpp
+++ Vector/for essential iterator.hpp
@@ -4,71 +4,65 @@
 #include "essential iterator.hpp"
 
 namespace mathvector {
-    namespace essential {
-        template<class T>
-        EssentialIterator<T>::essential_iterator(T* pointer):
-            work_with_pointer(pointer) {}
+    template<class T>
+    EssentialIterator<T>::EssentialIterator(T* pointer):
+        work_with_pointer(pointer) {}
 
-        template<class T>
-        EssentialIterator<T>::essential_iterator(const EssentialIterator &element):
-            work_with_pointer(element.pointer) {}
+    template<class T>
+    EssentialIterator<T>::EssentialIterator(const EssentialIterator &element):
+        work_with_pointer(element.pointer) {}
 
-        template<class T>
-        EssentialIterator<T>&essential_iterator<T>::operator = (const EssentialIterator &element) {
-            if (this != &element) {
-                pointer = element.pointer;
-            }
+    template<class T>
+    EssentialIterator<T>&EssentialIterator<T>::operator = (const EssentialIterator &element) {
+        if (this != &element)
+            pointer = element.pointer;
+        return *this;
+    }
 
-            return *this;
-        }
+    template<class T>
+    EssentialIterator<T>~EssentialIterator() {
+        pointer = nullptr;
+    }
 
-        template<class T>
-        EssentialIterator<T>~essential_iterator() {
-            pointer = nullptr;
-        }
+    template <class T>
+    EssentialIterator<T>&EssentialIterator<T>::operator++() {
+        ++this->pointer;
+        return *this;
+    }
 
-        template <class T>
-        EssentialIterator<T>&essential_iterator<T>::operator++() {
-            ++this->pointer;
+    template <class T>
+    EssentialIterator<T>&EssentialIterator<T>::operator--() {
+        --this->pointer;
+        return *this;
+    }
 
-            return *this;
-        }
+    template <class T>
+    EssentialIterator<T>EssentialIterator<T>::operator++(int) {
+        EssentialIterator buffer(*this);
+        this->operator++();
+        return buffer;
+    }
 
-        template <class T>
-        EssentialIterator<T>&essential_iterator<T>::operator--() {
-            --this->pointer;
+    template <class T>
+    EssentialIterator<T>EssentialIterator<T>::operator--(int) {
+        EssentialIterator buffer(*this);
+        this->operator--();
+        return buffer;
+    }
 
-            return *this;
-        }
+    template <class T>
+    bool EssentialIterator<T>::operator == (const EssentialIterator<T>&element) {
+        return pointer == element.pointer;
+    }
 
-        template <class T>
-        EssentialIterator<T>essential_iterator<T>::operator++(int) {
-            EssentialIterator buffer(*this);
-            this->operator++();
-            return buffer;
-        }
+    template <class T>
+    bool EssentialIterator<T>::operator != (const EssentialIterator<T>&element) {
+        return pointer != element.pointer;
+    }
 
-        template <class T>
-        EssentialIterator<T>essential_iterator<T>::operator--(int) {
-            EssentialIterator buffer(*this);
-            this->operator--();
-            return buffer;
-        }
-
-        template <class T>
-        bool EssentialIterator<T>::operator == (const EssentialIterator<T>&element) {
-            return pointer == element.pointer;
-        }
-
-        template <class T>
-        bool EssentialIterator<T>::operator != (const EssentialIterator<T>&element) {
-            return pointer != element.pointer;
-        }
-
-        template <class T>
-        ptrdiff_t EssentialIterator<T>::operator - (const EssentialIterator<T>&element) {
-            return (pointer - element.pointer);
-        }
+    template <class T>
+    ptrdiff_t EssentialIterator<T>::operator - (const EssentialIterator<T>&element) {
+        return pointer - element.pointer;
     }
 }
 
