e1b04204b179b8a41e6f26b41b35a63bb38e636e
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-05-21 00:04:16 +0300

    Added new functions and removed unnecessary functions

--- Vector/for vector.hpp
+++ Vector/for vector.hpp
@@ -167,7 +167,7 @@ namespace mathvector {
     }
 
     template <class T>
-    typename MathematicalVector<T>::for_nonconst MathematicalVector<T>::begin_nonconst() {
+    typename MathematicalVector<T>::for_nonconst MathematicalVector<T>::begin() {
             return NonConstantIterator(this->data);
     }
 
@@ -177,7 +177,7 @@ namespace mathvector {
     }
 
     template <class T>
-    typename MathematicalVector<T>::nonconst_iterator MathematicalVector<T>::end_nonconst() {
+    typename MathematicalVector<T>::nonconst_iterator MathematicalVector<T>::end() {
         return NonConstantIterator(this->data + this->size);
     }
 
@@ -250,7 +250,7 @@ namespace mathvector {
             end = (*this).end_const();
         }
 
-        typename MathematicalVector<T>::for_nonconst initial_result = result.begin_nonconst();
+        typename MathematicalVector<T>::for_nonconst initial_result = result.begin();
         for (typename MathematicalVector<T>::for_const it = begin; it != end; ++it) {
                 *initial_result += *it;
                 initial_result++;
@@ -260,7 +260,7 @@ namespace mathvector {
     }
 
     template <class T>
-    MathematicalVector<T>MathematicalVector<T>::vector_subtraction(const vector<T>& vector) {
+    MathematicalVector<T>MathematicalVector<T>::vector_subtraction(const MathematicalVector<T>& vector) {
         MathematicalVector<T> result = *this;
         typename MathematicalVector<T>::for_const begin = vector.begin_const();
         typename MathematicalVector<T>::for_const end = vector.end_const();
@@ -271,14 +271,47 @@ namespace mathvector {
             end = (*this).end_const();
         }
 
-        typename MathematicalVector<T>::for_nonconst initial_result = result.begin_nonconst();
+        typename MathematicalVector<T>::for_nonconst initial_result = result.begin();
         for (typename MathematicalVector<T>::for_const it = begin; it != end; ++it) {
                 *initial_result -= *it;
                 initial_result++;
         }
 
-            return result;
+        return result;
+    }
+
+    template <class T>
+    MathematicalVector<T> MathematicalVector<T>::vector_multiplication(const MathematicalVector<T>& vector) {
+        MathematicalVector<T> result(*this);
+
+        result[0] = (*this)[1] * vector[2] - (*this)[2] * vector[1];
+        result[1] = (*this)[2] * vector[0] - (*this)[0] * vector[2];
+        result[2] = (*this)[0] * vector[1] - (*this)[1] * vector[0];
+
+        return result;
+    }
+
+    template <class T>
+    T MathematicalVector<T>::scalar_multiplication(const MathematicalVector<T>& vector) {
+        MathematicalVector<T> result(*this);
+        typename MathematicalVector<T>::for_const begin = vector.begin_const();
+        typename MathematicalVector<T>::for_const end = vector.end_const();
+
+        if ((*this).size < vector.size) {
+            result = vector;
+            begin = (*this).begin_const();
+            end = (*this).end_const();
         }
+
+        T end_result = 0;
+        typename MathematicalVector<T>::for_nonconst initial_result = result.begin();
+        for (typename MathematicalVector<T>::for_const it = begin; it != end; ++it) {
+            end_result = (*initial_result) * (*it);
+            initial_result++;
+        }
+
+        return end_result;
+    }
 
     template <class T>
     MathematicalVector<T> MathematicalVector<T>::operator + (const MathematicalVector<T> &vector) {
@@ -292,7 +325,7 @@ namespace mathvector {
             end = (*this).end_const();
         }
 
-        typename MathematicalVector<T>::for_nonconst initial_result = result.begin_nonconst();
+        typename MathematicalVector<T>::for_nonconst initial_result = result.begin();
         for (typename MathematicalVector<T>::for_const it = begin; it != end; ++it){
             *initial_result += *it;
             initial_result++;
@@ -312,7 +345,7 @@ namespace mathvector {
             end = (*this).end_const();
         }
 
-        typename MathematicalVector<T>::for_nonconst initial_result = result.begin_nonconst();
+        typename MathematicalVector<T>::for_nonconst initial_result = result.begin();
         for (typename MathematicalVector<T>::for_const it = begin; it != end; ++it) {
             *initial_result -= *it;
             initial_result++;
@@ -324,180 +357,151 @@ namespace mathvector {
     template <class T>
     MathematicalVector<T>MathematicalVector<T>::operator * (const MathematicalVector<T> &vector) {
         MathematicalVector<T> result(*this);
-        typename MathematicalVector<T>::const_iterator begin = vector.begin_const();
-        typename MathematicalVector<T>::const_iterator end = vector.end_const();
+        typename MathematicalVector<T>::for_const begin = vector.begin_const();
+        typename MathematicalVector<T>::for_const end = vector.end_const();
 
         if ((*this).size < vector.size) {
             result = vector;
             begin = (*this).begin_const();
             end = (*this).end_const();
         }
-        typename MathematicalVector<T>::for_nonconst initial_result = result.begin_nonconst();
+
+        typename MathematicalVector<T>::for_nonconst initial_result = result.begin();
         for (typename MathematicalVector<T>::for_const it = begin; it != end; ++it) {
             *initial_result *= *it;
             initial_result++;
         }
+
         return result;
     }
 
-        template <class T>
-        MathematicalVector<T>&MathematicalVector<T>::operator += (const MathematicalVector &vector) {
-            return *this = operator + (*this, vector);
-        }
+    template <class T>
+    MathematicalVector<T>MathematicalVector<T>::operator * (const MathematicalVector<T>& vector) {
+        MathematicalVector<T> result(*this);
 
-        template <class T>
-        MathematicalVector<T>&MathematicalVector<T>::operator -= (const MathematicalVector &vector) {
-            return *this = operator - (*this, vector);
-        }
+        result[0] = (*this)[1] * vector[2] - (*this)[2] * vector[1];
+        result[1] = (*this)[2] * vector[0] - (*this)[0] * vector[2];
+        result[2] = (*this)[0] * vector[1] - (*this)[1] * vector[0];
 
-
-
-
-        template <class T>
-        MathematicalVector<T>&MathematicalVector<T>::operator *= (const MathematicalVector &vector) {
-            return *this = operator * (*this, vector);
-        }
-
-
-        template <class T>
-        vector<T>& vector<T>::operator^=(const vector<T>& other_vector){
-            return *this = operator^(*this, other_vector);
-        }
-
-        template <class T>
-        vector<T> vector<T>::operator*(const T& some_value){
-            vector<T> RESULT(*this);
-
-            for (typename vector<T>::noconst_it res_it = RESULT.begin(); res_it != RESULT.end(); ++res_it){
-                (*res_it) *= some_value;
-            }
-
-            return RESULT;
-        }
-
-        template <class T>
-        vector<T> operator*(const T& some_value, const vector<T>& V){
-            vector<T> RESULT(V);
-
-            for (typename vector<T>::noconst_it res_it = RESULT.begin(); res_it != RESULT.end(); ++res_it){
-                (*res_it) *= some_value;
-            }
-
-            return RESULT;
-        }
-
-        template <class T>
-        vector<T> operator*(const vector<T>& V, const T& some_value){
-            vector<T> RESULT(V);
-
-            for (typename vector<T>::noconst_it res_it = RESULT.begin(); res_it != RESULT.end(); ++res_it){
-                (*res_it) *= some_value;
-            }
-
-            return RESULT;
-        }
-
-
-        template <class T>
-        vector<T> vector<T>::operator/(const T& some_value){
-            vector<T> RESULT(*this);
-
-            for (typename vector<T>::noconst_it res_it = RESULT.begin(); res_it != RESULT.end(); ++res_it){
-                (*res_it) /= some_value;
-            }
-
-            return RESULT;
-        }
-
-        template <class T>
-        vector<T>& vector<T>::operator*=(const T& some_value){
-            return *this = operator*(some_value);
-        }
-
-        template <class T>
-        vector<T>& vector<T>::operator/=(const T& some_value){
-            return *this = operator/(*this, some_value);
-        }
-
-        template <class T>
-        bool vector<T>::is_ort2(const vector<T>& other_vector){
-            if ((*this) ^ other_vector == 0)
-                return true;
-            return false;
-        }
-
-        template <class T>
-        bool vector<T>::is_ort3(const vector<T>& V1, const vector<T>& V2) const {
-            if (V1 ^ V2 == 0 && V2 ^ (*this) == 0 && V1 ^ (*this) == 0)
-                return true;
-            return false;
-        }
-
-        template <class T>
-        bool operator==(const vector<T>& lhs, const vector<T>& rhs){
-
-            bool are_equal = lhs.size == rhs.size;
-
-            if (are_equal) {
-
-                for (size_t i = 0; i < lhs.size && are_equal; ++i) {
-
-                    if (lhs[i] != rhs[i]) {
-                        are_equal = false;
-                    }
-                }
-            }
-
-            return are_equal;
-        }
-
-        template <class T>
-        bool operator!=(const vector<T>& lhs, const vector<T>& rhs){
-            return !(rhs == lhs);
-        }
-
-        template <class T>
-        bool operator<(const vector<T>& lhs, const vector<T>& rhs){
-            bool is_less = lhs.size < rhs.size;
-
-            if (!is_less && lhs.size == rhs.size) {
-
-                for (size_t i = 0; i < lhs.size && !is_less; ++i) {
-
-                    if (lhs[i] < rhs[i]) {
-                        is_less = true;
-                    }
-                }
-            }
-
-            return is_less;
-        }
-
-        template <class T>
-        bool operator<=(const vector<T>& lhs, const vector<T>& rhs){
-            return lhs < rhs || lhs == rhs;
-        }
-
-        template <class T>
-        bool operator>(const vector<T>& lhs, const vector<T>& rhs){
-            return !(lhs < rhs) && !(lhs == rhs);
-        }
-
-        template <class T>
-        bool operator>=(const vector<T>& lhs, const vector<T>& rhs){
-            return lhs > rhs || lhs == rhs;
-        }
-
-        template <class T>
-        std::ostream& operator<<(std::ostream& stream, const vector<T>& init_vector){
-            for (typename vector<T>::const_it iterator = init_vector.cbegin(); iterator != init_vector.cend(); ++iterator) {
-                std::cout << *iterator << " ";
-            }
-
-            return stream;
-        }
+        return result;
     }
 
+    template <class T>
+    T MathematicalVector<T>::operator ^ (const MathematicalVector<T>& vector) {
+        MathematicalVector<T> result(*this);
+        typename MathematicalVector<T>::for_const begin = vector.begin_const();
+        typename MathematicalVector<T>::for_const end = vector.end_const();
+
+        if ((*this).size < vector.size) {
+            result = vector;
+            begin = (*this).begin_const();
+            end = (*this).end_const();
+        }
+
+        T end_result = 0;
+        typename MathematicalVector<T>::for_nonconst initial_result = result.begin();
+        for (typename MathematicalVector<T>::for_const it = begin; it != end; ++it) {
+            end_result = (*initial_result) * (*it);
+            initial_result++;
+        }
+
+        return end_result;
+    }
+
+    template <class T>
+    MathematicalVector<T>&MathematicalVector<T>::operator += (const MathematicalVector<T> &vector) {
+        return *this = operator + (*this, vector);
+    }
+
+    template <class T>
+    MathematicalVector<T>&MathematicalVector<T>::operator -= (const MathematicalVector<T> &vector) {
+        return *this = operator - (*this, vector);
+    }
+
+    template <class T>
+    MathematicalVector<T>&MathematicalVector<T>::operator *= (const MathematicalVector<T> &vector) {
+            return *this = operator * (*this, vector);
+    }
+
+    template <class T>
+    MathematicalVector<T>& MathematicalVector<T>::operator *= (const T& number_value) {
+        return *this = operator * (number_value);
+    }
+
+    template <class T>
+    bool MathematicalVector<T>::is_orthogonal(const MathematicalVector<T>& vector) const {
+        if ((*this) ^ vector == 0)
+            return true;
+        return false;
+    }
+
+    template <class T>
+    bool MathematicalVector<T>::is_collinear(const MathematicalVector<T>& vector) const {
+        MathematicalVector<T> result(*this);
+        typename MathematicalVector<T>::for_const begin = vector.begin_const();
+        typename MathematicalVector<T>::for_const end = vector.end_const();
+
+        if ((*this).size < vector.size) {
+            result = vector;
+            begin = (*this).cbegin();
+            end = (*this).cend();
+        }
+
+        float for_result = 0;
+        float buffer = 0;
+
+        typename MathematicalVector<T>::for_nonconst initial_result = result.begin();
+
+        for (typename MathematicalVector<T>::for_const it = begin; it != end; ++it) {
+            buffer = for_result;
+            for_result = *initial_result / *it;
+
+            if (for_result != buffer)
+                return false;
+
+            initial_result++;
+        }
+
+        return true;
+    }
+
+    template <class T>
+    bool operator == (const MathematicalVector<T>& first_operand, const MathematicalVector<T>& second_operand) {
+        return first_operand == second_operand;
+    }
+
+    template <class T>
+    bool operator != (const MathematicalVector<T>& first_operand, const MathematicalVector<T>& second_operand) {
+        return !(first_operand == second_operand);
+    }
+
+    template <class T>
+    bool operator < (const MathematicalVector<T>& first_operand, const MathematicalVector<T>& second_operand) {
+        return !(first_operand > second_operand) && !(first_operand == second_operand);
+    }
+
+    template <class T>
+    bool operator <= (const MathematicalVector<T>& first_operand, const MathematicalVector<T>& second_operand) {
+        return first_operand < second_operand || first_operand == second_operand;
+    }
+
+    template <class T>
+    bool operator >= (const MathematicalVector<T>& first_operand, const MathematicalVector<T>& second_operand) {
+        return first_operand > second_operand || first_operand == second_operand;
+    }
+
+    template <class T>
+    bool operator > (const MathematicalVector<T>& first_operand, const MathematicalVector<T>& second_operand) {
+        return !(first_operand < second_operand) && !(first_operand == second_operand);
+    }
+
+    template <class T>
+    std::ostream& operator<<(std::ostream& stream, const MathematicalVector<T>& vector) {
+        for (typename MathematicalVector<T>::for_const it = vector.begin_const(); it != vector.end_const(); ++it) {
+            std::cout << *it << " ";
+        }
+        return stream;
+    }
 }
 
-#endif FOR_VECTOR_HPP
+#endif // FOR_VECTOR_HPP
