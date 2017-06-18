c8a35bcee36558dc9cceb55b93d4642f39b7f874
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-06-19 00:18:40 +0300

    Added files for controller and lift classes

---
+++ Elevator/controller.h
@@ -0,0 +1,16 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include <QObject>
+#include <QWidget>
+#include <QVBoxLayout>
+
+#include "buttons.h"
+#include "times.h"
+
+class Controller {
+public:
+    Controller();
+};
+
+#endif // CONTROLLER_H
