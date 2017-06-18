a5999f6b9695a0b1c38d1e6c4a09e17e03a924f7
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-06-18 23:37:14 +0300

    Added constructor

--- Elevator/cabin.cpp
+++ Elevator/cabin.cpp
@@ -1,6 +1,13 @@
 #include "cabin.h"
 
-cabin::cabin()
-{
+Cabin::Cabin(QObject *parent) {
+    current_state = FREE;
+    current_floor = 0;
+    needed_floor = 0;
 
+    current_motion = STOPPED;
+    QObject::connect(&doors, SIGNAL(doorIsClosed()), this, SLOT(changeToMoving()));
+    QObject::connect(&timer_for_motion, SIGNAL(timeout()), this, SLOT(changeToMoving()));
+    QObject::connect(this, SIGNAL(changeToArrivedAtFloor()), this, SLOT(changeToFree()));
+    QObject::connect(this, SIGNAL(changeToMove()), this, SLOT(changeToMoving()));
 }
