b3ff0c6bf29887cdfb9e91716c2dcfc945e3ccff
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-06-19 00:26:37 +0300

    Added controller class with signals and slots

--- Elevator/controller.h
+++ Elevator/controller.h
@@ -8,9 +8,30 @@
 #include "buttons.h"
 #include "times.h"
 
-class Controller {
+class Controller : public QWidget {
+    Q_OBJECT
+    enum controller_state {
+        NOTHING,
+        BUSY
+    };
 public:
-    Controller();
+    explicit Controller(QWidget *parent = 0);
+    ~Controller();
+signals:
+    void SendNewNeededFloor(int new_floor);
+public slots:
+    void changeToCurrentFloor(int floor, motion motion);
+    void slotAchieveFloor(int floor, motion motion);
+private slots:
+    void changeToAddNewFloor(int new_floor);
+private:
+    bool getNewNeededFloor(int &new_floor);
+    bool         needed_floors[FLOOR_NUMBERS];
+    Buttons*     buttons[FLOOR_NUMBERS];
+    int          current_floor;
+    motion   current_motion;
+    QVBoxLayout* layout;
+    controller_state current_state;
 };
 
 #endif // CONTROLLER_H
