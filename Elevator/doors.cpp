0080f7b47770faff927273bce69c0605f7411502
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-06-18 22:23:20 +0300

    Added slot for open door

--- Elevator/doors.cpp
+++ Elevator/doors.cpp
@@ -8,3 +8,11 @@ Doors::Doors() {
     QObject::connect(&timer_for_close, SIGNAL(timeout()), this, SLOT(changeToClose()));
     QObject::connect(&timer_for_wait, SIGNAL(timeout()), this, SLOT(changeToClosing()));
 }
+
+void Doors::changeToOpen() {
+    if (current_state == OPENING) {
+        current_state = OPEN;
+        qDebug() << "✨ Doors opened! I ask to go inside the elevator! ✨";
+        timerWaitOpen.start(TIME_DOOR_WAIT);
+    }
+}
