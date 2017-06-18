1cc0714368b6bd000f992e4822d62ea0e6b565a4
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-06-19 00:55:00 +0300

    Added lift class

--- Elevator/lift.cpp
+++ Elevator/lift.cpp
@@ -1,6 +1,14 @@
 #include "lift.h"
 
-lift::lift()
-{
+Lift::Lift(QObject *parent) : QObject(parent) {
+    QObject::connect(&elevator_cabin, SIGNAL(previousFloor(int, motion)),
+                     &control, SLOT(changeToCurrentFloor(int, motion));
+    QObject::connect(&elevator_cabin, SIGNAL(selectedFloor(int, motion)),
+                     &control, SLOT(changeToNeededFloor(int,motion)));
+    QObject::connect(&control, SIGNAL(SendNewNeededFloor(int)),
+                     &elevator_cabin, SLOT(changeToBusy(int));
+}
 
+QWidget *Lift::GetWidget() {
+    return &control;
 }
