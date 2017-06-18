7d3d14593315daa3424ef2c7d54dbef9c5f85d6f
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-06-18 22:28:56 +0300

    Added slot for opening door

--- Elevator/doors.cpp
+++ Elevator/doors.cpp
@@ -13,6 +13,31 @@ void Doors::changeToOpen() {
     if (current_state == OPENING) {
         current_state = OPEN;
         qDebug() << "✨ Doors opened! I ask to go inside the elevator! ✨";
-        timerWaitOpen.start(TIME_DOOR_WAIT);
+        timer_for_wait.start(TIME_DOOR_WAIT);
     }
 }
+
+void Doors::changeToOpening() {
+    if (current_state == CLOSE) {
+        state = OPENING;
+        qDebug() << "✨ Doors is opening! Wait a bit! ✨";
+        timer_for_open.start(TIME_DOOR);
+    }
+
+    if (current_state == CLOSING) {
+        current_state = OPENING;
+        qDebug() << "✨ Doors is opening! Wait a bit! ✨";
+        int time = timer_for_close.remainingTime();
+        timer_for_close.stop();
+        timer_for_open.start(TIME_DOOR - time);
+    }
+}
+
+void Doors::changeToClose() {
+    if(state == CLOSING) {
+        state = CLOSE;
+        qDebug() << "doors closed";
+        emit DoorIsClosed();
+    }
+}
+
