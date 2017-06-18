d3f73dcdbcc308afd21fe9e84eeae2e9416ea008
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-06-19 00:30:31 +0300

    Added constrcutor

--- Elevator/controller.cpp
+++ Elevator/controller.cpp
@@ -1,6 +1,14 @@
 #include "controller.h"
 
-Controller::Controller()
-{
-
+Controller::Controller(QWidget *parent) : QWidget(parent) {
+    layout = new QVBoxLayout;
+    this->setLayout(layout);
+    for (int i = 0; i < FLOOR_NUMBERS; i++) {
+        buttons[i] = new Button;
+        layout->addWidget(dynamic_cast<QPushButton*>(buttons[i]));
+        buttons[i]->setText(QString::number(i));
+        buttons[i]->set_floor(i);
+        needed_floors[i] = false;
+        QObject::connect(buttons[i], SIGNAL(changeFloor(int)), this, SLOT(changeToAddNewFloor(int)));
+    }
 }
